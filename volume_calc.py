3d718a6e932828a57252cdbfcc503ed141c06b8a
kimjuyeonstella <163102954+kimjuyeonstella@users.noreply.github.com>
2024-03-28 23:39:37 +0900

    Update volume_calc.py

--- volume_calc.py
+++ volume_calc.py
@@ -2,4 +2,20 @@ width=float(input("가로:"))
 length=float(input("세로:"))
 height=float(input("높이:"))
 volume=width*length*height
+total_length=length+width+height
+
 print("박스의 부피는",volume,"입니다")
+
+if (total_length<=80):
+   charge=5000
+
+elif(total_length<=100):
+   charge=8000
+
+elif(total_length<=120):
+   charge=10000 
+else:
+    charge=13000
+
+print("Total length:",total_length)
+print("Charge",charge)
